6f26471f72d2a06ca7d9158bf0ca2c0ced33b19f
staubracing <staubracing@gmail.com>
2024-11-03 11:09:25 -0600

    email check box

--- Project_5-Bonus_forms/app.js
+++ Project_5-Bonus_forms/app.js
@@ -21,4 +21,23 @@ function activateButton() {
 
   checkInputs();
 }
+
+// Function to activate email input when checkbox is clicked
+function activateEmailInput() {
+  const form = document.getElementById('myForm');
+  const checkbox = form.querySelector('input[id="emailCheckbox"]');
+  const emailInput = form.querySelector('input[id="email"]');
+
+  emailInput.style.display = checkbox.checked ? 'block' : 'none';
+
+  checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+      emailInput.style.display = 'block';
+    } else {
+      emailInput.style.display = 'none';
+    }
+  });
+}
+
 document.addEventListener('DOMContentLoaded', activateButton);
+document.addEventListener('DOMContentLoaded', activateEmailInput);
